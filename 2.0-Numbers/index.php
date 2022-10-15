ee32dfba4bf72c05c25e1ac58f8b554cad49bc7b
alhariri <19abdul97@gmail.com>
2022-10-15 23:16:18 +0100

    is_numeric function

--- 2.0-Numbers/index.php
+++ 2.0-Numbers/index.php
@@ -1,3 +1,38 @@
 <?php
-echo "Hello numbers!";
-echo "<pre>"; 
+//1 Numbers
+// numbers methodes 
+echo "<br>";
+echo is_numeric(9); // prints 1
+echo "<br>";
+echo is_numeric("9"); // prints 1 
+echo "<br>";
+echo is_numeric(0.9); // prints 1
+echo "<br>";
+echo is_numeric("l"); // prints nothing which evaluates to false
+echo "<br>";
+echo is_numeric("10e200");
+echo "<br>";
+
+$num1 = "100,229,399";
+$num2 = str_replace( ",", "",$num1);
+echo "<br>";
+echo $num2;
+echo "<br>";
+echo is_numeric($num2);  // 1 ===> True
+
+echo "<br>";
+// 2.2 comparing floating points¬
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
