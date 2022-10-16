f43c91922bf8597ce3a2b0b27a55c6a7014a94f7
alhariri <19abdul97@gmail.com>
2022-10-16 22:25:12 +0100

    round, floor, ceil ...

--- 2.0-Numbers/index.php
+++ 2.0-Numbers/index.php
@@ -22,11 +22,33 @@ echo is_numeric($num2);  // 1 ===> True
 
 echo "<br>";
 // 2.2 comparing floating points¬
+$delta = 0.00001;
+$a = 1.0000001;
+$b = 1.0000000;
+echo $delta;
 echo "<br>";
 echo "<br>";
+echo $a - $b;
 echo "<br>";
+echo abs($a - $b);
+if(abs($a - $b) < $delta){
+    echo 2;
+}
 echo "<br>";
+// 2.3 Rounding floating points
 echo "<br>";
+echo round(2.4); // 2
+echo "<br>";
+echo round(2.5); // 3 
+echo "<br>";
+echo round(2.53838, 2); // 2.54 and that's expected and normal !
+echo "<br>";
+echo ceil(2.4); // 3
+echo "<br>";
+echo floor(2.4); //2 
+// just like Javascript :)
+echo "<br>";
+// 2.4 operating on a series on ints
 echo "<br>";
 echo "<br>";
 echo "<br>";
